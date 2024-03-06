bb3c3a23030970187f749b25d7ef27db05826f2e
Julia <jsgoransson@gmail.com>
2024-03-06 14:50:34 +0100

    styling edit form

--- AutoHub/resources/views/edit.blade.php
+++ AutoHub/resources/views/edit.blade.php
@@ -1,23 +1,66 @@
-<form method="POST" action="{{ route('posts.update', ['id' => $post->id]) }}" enctype="multipart/form-data">
-    @csrf
-    @method('PUT')
-    <label for="title">Title</label><br>
-    <input type="text" id="title" name="title" value="{{$post->title}}"><br>
+@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-    <label for="brand">Brand</label><br>
-    <input type="text" id="brand" name="brand" value="{{$post->brand}}"><br>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
-    <label for="model">Model</label><br>
-    <input type="text" id="model" name="model" value="{{$post->model}}"><br>
+<header class="w-full h-20 flex justify-around items-center shadow">
+    <div class="text-xl">
+        <a href="">Go back</a>
+    </div>
+    <div class="text-4xl">
+        <h1>AutoHub</h1>
+    </div>
+    <div class="text-xl">
+        <p><a href="{{ route('logout') }}">Log out</a></p>
+    </div>
+</header>
 
-    <label for="model_year">Model year</label><br>
-    <input type="number" id="model_year" name="model_year" min="1900" max="2024" step="1" value="{{$post->model_year}}" /><br> <!-- CHANGE TO YEAR PICKER WITH JS-->
+<body class="flex flex-col items-center">
+    <div class="text-xl mt-10 mb-5">
+        <h2>Update post</h2>
+    </div>
 
-    <label for="description">Description</label><br>
-    <textarea id="description" name="description" rows="4" cols="50">{{$post->description}}</textarea>
+    <form class="flex flex-col max-w-md" method="POST" action="{{ route('posts.update', ['id' => $post->id]) }}" enctype="multipart/form-data">
+        @csrf
+        @method('PUT')
+        <div>
+            <label class="font-bold" for="title">Title</label><br>
+            <input class="w-full p-1 mb-3 bg-slate-100 rounded" type="text" id="title" name="title" value="{{$post->title}}">
+        </div>
 
-    <label for="car_img">Picture</label>
-    <input type="file" name="car_img" id="car_img">
-    <br>
-    <input type="submit" value="Update">
-</form>
+        <div class="flex gap-5">
+            <div class="w-1/2">
+                <label class="font-bold" for="brand">Brand</label><br>
+                <input class="w-full p-1 mb-3 bg-slate-100 rounded" type="text" id="brand" name="brand" value="{{$post->brand}}">
+            </div>
+            <div class="w-1/2">
+                <label class="font-bold" for="model">Model</label><br>
+                <input class="w-full p-1 mb-3 bg-slate-100 rounded" type="text" id="model" name="model" value="{{$post->model}}">
+            </div>
+        </div>
+        <div class="flex gap-5">
+            <div class="w-1/2">
+                <label class="font-bold" for="model_year">Model year</label>
+                <input class="w-full p-1 mb-3 bg-slate-100 rounded" type="number" id="model_year" name="model_year" min="1900" max="2024" step="1" value="{{$post->model_year}}" /> <!-- CHANGE TO YEAR PICKER WITH JS-->
+            </div>
+            <div class="w-1/2">
+                <label class="font-bold" for="car_img">Picture</label>
+                <input class="w-full" type="file" name="car_img" id="car_img">
+            </div>
+        </div>
+        <div>
+            <label class="font-bold" for="description">Description</label>
+            <textarea class="w-full p-1 mb-3 bg-slate-100 rounded" id="description" name="description" rows="4" cols="50">{{$post->description}}</textarea>
+        </div>
+        <div class="flex w-full justify-center">
+            <input class="font-semibold text-white w-1/4 p-2 bg-sky-500 rounded-lg hover:bg-sky-600 cursor-pointer" type="submit" value="Update">
+        </div>
+    </form>
+</body>
+
+</html>
